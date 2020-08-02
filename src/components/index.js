17e9953f147414f25b3756f40a83985c7825df97
Utkarsh Deep <utkarsh_d@hdfclife.com>
2020-08-02 21:41:51 +0530

    status style changes

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Cell from './Cell'
 import styles from './styles.scss'
 import { connect } from 'react-redux'
-import { APP_STATUS } from '../constants'
+import { APP_STATUS, GAME_STATUS_CLASS } from '../constants'
 
 function App ({ cellData, score, hint, status }) {
   const isGameOver = status === APP_STATUS.GAME_OVER
+  const gameStatusClass = styles[GAME_STATUS_CLASS[status]]
   return (
     <div className={styles.container}>
-      <div className={styles.message}>{isGameOver ? 'Final' : 'Your'} Score: {score}</div>
+      <div className={`${styles.message} ${gameStatusClass}`}>{isGameOver ? 'Final' : 'Your'} Score: {score}</div>
       <main className={styles.gridContainer}>
         {cellData.map((row, ri) => row.map((cell, ci) => (
           <Cell
--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,12 @@ export const APP_STATUS = {
   GAME_OVER: 'Game Over'
 }
 
+export const GAME_STATUS_CLASS = {
+  Ready: 'ready',
+  Playing: 'playing',
+  'Game Over': 'finished'
+}
+
 export const CELL_STATUS = {
   CLOSED: 'closed',
   OPEN: 'open'
