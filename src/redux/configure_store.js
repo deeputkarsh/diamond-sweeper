3bdbb86fed1a89d38a45bb5ba1dadca5617bf31b
Utkarsh Deep <utkarsh_d@hdfclife.com>
2020-08-02 15:08:11 +0530

    refactor wip

---
+++ src/redux/configure_store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import promiseMiddleware from 'redux-promise'
+
+import userStore from './userActions'
+
+const rootReducer = combineReducers({
+  userStore
+})
+let middleware = ''
+if (process.env.NODE_ENV === 'production') {
+  middleware = applyMiddleware(promiseMiddleware, thunk)
+} else {
+  middleware = applyMiddleware(promiseMiddleware, thunk, createLogger())
+}
+export const store = createStore(rootReducer, {}, middleware)
--- webpack.config.js
+++ webpack.config.js
@@ -1,150 +1,137 @@
-const path = require('path')
 const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const dotenv = require('dotenv')
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const rootDir = path.resolve(__dirname, '.')
+const distDir = path.resolve(__dirname, '.', 'dist')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const cssnano = require('cssnano')
 
-const sassLoader = [{
-  loader: 'sass-loader'
-}, {
-  loader: 'postcss-loader',
-  options: {
-    plugins: () => [
-      require('autoprefixer')({
-        flexbox: 'no-2009'
-      })
-    ]
-  }
-}]
-const scssLoader = {
-  test: /\.(s[ac]ss)$/,
-  exclude: /node_modules/,
-  use: [{
-    loader: MiniCssExtractPlugin.loader
-  }, {
-    loader: 'css-loader',
-    options: {
-      sourceMap: false,
-      modules: { localIdentName: '[name]-[local]_[hash:base64:5]' }
-    }
-  }, ...sassLoader]
-}
-const common = {
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-      chunkFilename: '[name].[hash].css'
-    })
-  ]
-}
-const commonModuleRules = [{
-  test: /\.jsx?$/,
-  loader: 'babel-loader',
-  include: [path.resolve(__dirname, 'src')],
-  query: {
-    presets: ['@babel/preset-env', '@babel/preset-react']
-  }
-}, {
-  test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|otf|svg)(\?[a-z0-9=.]+)?$/,
-  loader: 'url-loader'
-}]
-
-const clientConfig = {
-  module: {
-    rules: [
-      ...commonModuleRules,
-      {
+module.exports = () => {
+  const env = dotenv.config().parsed
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next])
+    return prev
+  }, {})
+  return {
+    context: rootDir,
+    entry: './src/index.js',
+    output: {
+      path: distDir,
+      publicPath: '/',
+      filename: '[name].[hash:8].js'
+    },
+    devServer: {
+      contentBase: rootDir,
+      publicPath: '/',
+      historyApiFallback: true,
+      hot: true,
+      open: true
+      // needed for phone testing
+      // host:'0.0.0.0',
+      // port:'8080'
+    },
+    devtool: 'cheap-module-source-map',
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader', // include eslint-loader,
+        include: path.resolve(__dirname, './', 'src')
+      }, {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|otf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader'
+      }, {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
-      },
-      scssLoader
-    ]
-  },
-  ...common,
-  mode: 'development',
-  entry: {
-    client: ['@babel/polyfill', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/client.js']
-  },
-  output: {
-    ...common.output,
-    hotUpdateChunkFilename: '.hot/[id].[hash].hot-update.js',
-    hotUpdateMainFilename: '.hot/[hash].hot-update.json'
-  },
-  name: 'client',
-  target: 'web',
-
-  optimization: {
-    splitChunks: {
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendor: {
-          chunks: 'initial',
-          name: 'vendor',
-          test: module => /node_modules/.test(module.resource),
-          enforce: true
+      }, {
+        test: /\.(scss|sass)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            // modules: true,
+            sourceMap: false,
+            modules: { localIdentName: '[name]-[local]_[hash:base64:5]' }
+          }
+        }, {
+          loader: 'sass-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [
+              require('autoprefixer')({
+                browsers: [
+                  '>1%',
+                  'last 4 versions',
+                  'Firefox ESR',
+                  'not ie < 9'
+                ],
+                flexbox: 'no-2009'
+              })
+            ]
+          }
+        }]
+      }]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({
+          cssProcessor: cssnano,
+          cssProcessorOptions: {
+            discardComments: {
+              removeAll: true
+            },
+            safe: true
+          },
+          canPrint: false
+        })
+      ],
+      splitChunks: {
+        minSize: 30000,
+        maxSize: 0,
+        minChunks: 1,
+        automaticNameDelimiter: '~',
+        name: true,
+        cacheGroups: {
+          default: false,
+          // vendors: false,
+          vendors: {
+            chunks: 'all',
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          common: {
+            chunks: 'async',
+            minChunks: 2,
+            name: 'common',
+            priority: -20,
+            reuseExistingChunk: true
+          }
         }
       }
-    }
-  },
-
-  devtool: 'cheap-module-source-map',
-
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  }
-}
-
-const serverConfig = {
-  module: {
-    rules: [
-      ...commonModuleRules,
-      {
-        ...scssLoader,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: false,
-              modules: {
-                localIdentName: '[name]-[local]_[hash:base64:5]'
-              }
-            }
-          },
-          ...sassLoader
-        ]
-      }
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        // where to find the html template
+        template: path.resolve(__dirname, 'index.html'),
+        // where to put the generated file
+        path: distDir,
+        favicon: './assets/favicon.ico',
+        // the output file name
+        filename: 'index.html',
+        files: {
+          css: ['styles.css'],
+          js: ['bundle.js']
+        }
+      }),
+      new webpack.DefinePlugin(envKeys)
     ]
-  },
-  ...common,
-  mode: 'development',
-  entry: {
-    server: ['@babel/polyfill', './src/server.js']
-  },
-  name: 'server',
-  target: 'node',
-  externals: [nodeExternals()],
-
-  devtool: 'cheap-module-source-map',
-
-  node: {
-    console: false,
-    global: false,
-    process: false,
-    Buffer: false,
-    __filename: false,
-    __dirname: false
   }
 }
-
-module.exports = [clientConfig, serverConfig]
